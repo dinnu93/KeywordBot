241b947a14f78d38e05c5a7f834e44d9bfe56cf3
Dinesh Padisetti <dinesh.padisetti@gmail.com>
2018-04-13 11:28:57 +0530

    Separated out the email functionality

---
+++ email.rb
@@ -0,0 +1,30 @@
+module Email
+  require 'mail'
+
+  OPTIONS = { address: "smtp.gmail.com",
+              port: 587,
+              user_name: ENV['GMAIL_USERNAME'],
+              password: ENV['GMAIL_PASSWORD'],
+              authentication: 'plain',
+              enable_starttls_auto: true
+            }
+
+  def Email.text_email(data)
+    res = ""
+    data.each do |post|
+      res << "\n#{post[:title]}\n#{post[:link]}\n"
+    end
+    res
+  end
+
+  def Email.send_email(data)
+    Mail.defaults { delivery_method :smtp, OPTIONS}
+
+    Mail.deliver do 
+      from ENV['GMAIL_USERNAME']
+      to ENV['GMAIL_USERNAME']
+      subject 'Here is the reddit feed you requested'
+      body Email.text_email(data)
+    end
+  end
+end
--- reddit.rb
+++ reddit.rb
@@ -1,6 +1,6 @@
 require 'httparty'
 require 'nokogiri'
-require 'mail'
+require_relative 'email'
 
 HEADERS = { 'User-agent': 'KeywordBot App' }
 
@@ -24,33 +24,6 @@ def get_data(search_url)
   end
 end
 
-def text_email(data)
-  res = ""
-  data.each do |post|
-    res << "\n#{post[:title]}\n#{post[:link]}\n"
-  end
-  res
-end
-
-OPTIONS = { address: "smtp.gmail.com",
-            port: 587,
-            user_name: ENV['GMAIL_USERNAME'],
-            password: ENV['GMAIL_PASSWORD'],
-            authentication: 'plain',
-            enable_starttls_auto: true
-          }
-
-def send_email(data)
-  Mail.defaults { delivery_method :smtp, OPTIONS}
-
-  Mail.deliver do 
-    from ENV['GMAIL_USERNAME']
-    to ENV['GMAIL_USERNAME']
-    subject 'Here is the reddit feed you requested'
-    body text_email(data)
-  end
-end
-
 begin 
   while true do
     keyword = "ruby on rails" 
@@ -58,7 +31,7 @@ begin
     search_url = "https://www.reddit.com/search?q=#{subreddit}#{keyword}&t=day"
     data = get_data(search_url)
     puts "Sending the email..."
-    send_email(data)
+    Email.send_email(data)
     a_day = 24*60*60
     sleep a_day
   end
